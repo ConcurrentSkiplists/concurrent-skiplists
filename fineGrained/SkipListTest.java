55828d4eedc7017c440822a7a93fc42d8eb343f9
Nishanth Shanmugham <nishanths@utexas.edu>
2016-11-12 18:44:44 -0600

    Add testing

---
+++ fineGrained/SkipListTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+public class SkipListTest {
+    @org.junit.Test
+    public void testSkipListSomething() throws Exception {
+        int got = 1;
+        int expected = 1;
+        assertEquals(expected, got);
+    }
+}
