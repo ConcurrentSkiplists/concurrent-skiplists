1c4705525c2a41565e8ec61d272a0c8e32236520
rahuljaisimha <rahul@jaisimha.com>
2016-11-13 22:51:26 -0600

    Added standard library implementation

---
+++ standardLibrary/SkipList.java
@@ -0,0 +1,32 @@
+package standardLibrary;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public final class SkipList implements skipListInterface.SkipListInterface {
+	ConcurrentSkipListMap<String, Integer> map = new ConcurrentSkipListMap<>();
+	public SkipList() {
+	}
+	
+	public boolean add(String key, Integer value) {
+		if(map.put(key, value) == null){
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean remove(String key) {
+		if(map.remove(key) == null){
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public Integer get(String key) {
+		return map.get(key);
+	}
+}
+	
+	
