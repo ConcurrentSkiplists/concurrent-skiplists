2cbb3c435454577051388205536e87b43e644d9f
Nishanth Shanmugham <nishanths@utexas.edu>
2016-11-15 22:15:17 -0600

    benchmarks: WIP

--- benchmarks/src/Benchmark.java
+++ benchmarks/src/Benchmark.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class Benchmark {
     static int[] NUM_THREADS = new int[]{1, 10, 100, 1000, 10000};
     static int[] NUM_OPS = new int[]{1000, 10000, 100000};
-    static TreeMap<Double, Op> t;
-    static WeightedOp weightedOp;
 
+    static int AVERAGE_OVER = 10;
+
+    static TreeMap<Double, Op> WEIGHTS;
     static List<skipListInterface.SkipListInterface> sl = new ArrayList<>();
 
     static void setOpWeights() {
-        t = new TreeMap<>();
-        t.put(0.9, Op.Get);
-        t.put(0.09, Op.Add);
-        t.put(0.01, Op.Remove);
-        weightedOp = new WeightedOp(new Random(System.nanoTime()), t);
+        WEIGHTS = new TreeMap<>();
+        WEIGHTS.put(0.9, Op.Get);
+        WEIGHTS.put(0.09, Op.Add);
+        WEIGHTS.put(0.01, Op.Remove);
     }
 
     public static void main(String args[]) {
@@ -25,6 +25,40 @@ public class Benchmark {
     static void runAll() {
         for (int numThreads : NUM_THREADS) {
             for (int numOps: NUM_OPS) {
+                makeData(numThreads, numOps);
+            }
+        }
+    }
+
+    static void makeData(int numThreads, int numOps) {
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < numThreads; i++) {
+            Thread t = new Thread(new Runner(numOps, WEIGHTS));
+            threads.add(t);
+        }
+    }
+}
+
+class Runner implements Runnable {
+    int numOps;
+    WeightedOp wop;
+
+    public Runner(int numOps, NavigableMap<Double, Op> weights) {
+        this.numOps = numOps;
+        this.wop = new WeightedOp(new Random(System.nanoTime()), weights);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < numOps; i++) {
+            switch (this.wop.next()) {
+                case Remove:
+                    break;
+                case Add:
+                    break;
+                case Get:
+                    break;
             }
         }
     }
